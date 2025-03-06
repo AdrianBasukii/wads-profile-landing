cb1098d7b6ac912a7c3f545d82ffcc935e19aa4f
AdrianBasukii <adriannugrohobasuki@gmail.com>
2025-03-06 11:36:37 +0700

    PFP image

--- src/components/profilecontainer.jsx
+++ src/components/profilecontainer.jsx
@@ -2,7 +2,7 @@ export default function ProfileContainer(){
     return(
         <div className="grid grid-rows-2 md:grid-cols-2 w-full md:max-w-[900px] h-[400px] gap-8">
             <div className="w-full h-full p-4 md:row-span-full">
-                <img className="h-full w-full object-cover rounded-sm" src="https://cdn.discordapp.com/attachments/1189821702826377286/1345076547467346062/image.png?ex=67c33ba7&is=67c1ea27&hm=9a88123a2972f4774a73d2c6f4f29da35d97ec6feaf850aa95f9d11da3794434&" alt="Profile Image" />
+                <img className="h-full w-full object-cover rounded-sm" src="src/assets/pfp.png" alt="Profile Image" />
             </div>
             <div className="w-full h-full p-2 md:row-span-full flex flex-col gap-12">
                 <h1 className="text-4xl px-2 md:text-6xl font-bold">Hi, I'm Adrian Basuki</h1>
